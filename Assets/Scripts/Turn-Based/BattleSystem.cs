8b505b5be8fd80a7ded98637c9242815abad8e51
05eze <esintim2022@elam.co.uk>
2024-03-12 11:50:07 +0000

    Battle Script
    20.42

--- Assets/Scripts/Turn-Based/BattleSystem.cs
+++ Assets/Scripts/Turn-Based/BattleSystem.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Text = TMPro.TextMeshProUGUI;
 
 public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST }
 
@@ -17,22 +19,62 @@ public class BattleSystem : MonoBehaviour
     Unit playerUnit;
     Unit enemyUnit;
 
+    public Text dialogueText;
+
+    public BattleHUD playerHUD;
+    public BattleHUD enemyHUD;
+    
     public BattleState state;
 
 
     void Start()
     {
         state = BattleState.START;
-        SetupBattle();
+        StartCoroutine(SetupBattle());
     }
 
-    void SetupBattle()
+    IEnumerator SetupBattle()
     {
         GameObject playerGO = Instantiate(playerPrefab, playerBattleStation);
         playerUnit = playerGO.GetComponent<Unit>();
         
         GameObject enemyGO = Instantiate(enemyPrefab, enemyBattleStation);
         enemyUnit = enemyGO.GetComponent<Unit>();
+
+        dialogueText.text = enemyUnit.unitName + " approaches!";
+
+        playerHUD.SetHUD(playerUnit);
+        enemyHUD.SetHUD(enemyUnit);
+
+        //Wait a few seconds before the PLAYER can do anything
+        yield return new WaitForSeconds(2F);
+
+        state = BattleState.PLAYERTURN;
+        PlayerTurn();
     }
    
+
+    IEnumerator PlayerAttack()
+    {
+        //Damage the enemy + wait for a few seconds
+        yield return new WaitForSeconds(2f);
+
+        //Check if the enemy is dead
+        //Change state based on what happened 
+    }
+    //The PLAYER's Turn
+    void PlayerTurn()
+    {
+        dialogueText.text = "Choose an action:";
+    }
+
+    public void OnAttackButton()
+    {
+        //Check if it is the PLAYER's turn
+        if (state != BattleState.PLAYERTURN)
+            return;
+
+        StartCoroutine(PlayerAttack());
+
+    }
 }

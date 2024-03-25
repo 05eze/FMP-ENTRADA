e36b4deebcb0fdc54ec404ea1053fed4ef82b6c6
05eze <esintim2022@elam.co.uk>
2024-03-25 11:50:03 +0000

    Add force forward

--- Assets/Scripts/Minigames/Dodge/CruzController.cs
+++ Assets/Scripts/Minigames/Dodge/CruzController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class CruzController : MonoBehaviour
 {
+    public Rigidbody rb;
+    //public float speed;
+    public float jumpHeight;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,12 @@ public class CruzController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        rb.velocity = Vector3.right * 10f;
+        rb.velocity = Vector3.down * 5f;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpHeight);
+        }
     }
 }

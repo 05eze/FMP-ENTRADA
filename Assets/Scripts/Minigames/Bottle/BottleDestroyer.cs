2c087869865e11d26e30c7256a15f9a7e3347d8f
05eze <esintim2022@elam.co.uk>
2024-04-16 09:50:04 +0100

    Updates to bottle destroyer script

--- Assets/Scripts/Minigames/Bottle/BottleDestroyer.cs
+++ Assets/Scripts/Minigames/Bottle/BottleDestroyer.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 public class BottleDestroyer : MonoBehaviour
 {
     
+    
     void OnTriggerEnter(Collider other)
     {
+        //Box Collector 
         if (gameObject.tag == "Player")
         {
             Destroy(gameObject);
         }
+        //GroundDetector
+        {
+
+        }
     }
+
 }

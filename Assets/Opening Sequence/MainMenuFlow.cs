49a44cd78afee9d634f0e2590fc81ea9fa412291
05eze <esintim2022@elam.co.uk>
2024-04-18 12:01:05 +0100

    Cuurs

--- Assets/Opening Sequence/MainMenuFlow.cs
+++ Assets/Opening Sequence/MainMenuFlow.cs
@@ -2,30 +2,62 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MainMenuFlow : MonoBehaviour
 {
     //public Animator cam;
+    
+    
     public GameObject beginGame, beginGameText;
     public GameObject buttons;
+    public GameObject logo;
 
+
+    //public GameObject optionsMenu;
+
+
+    private void Start()
+    {
+        StartCoroutine("StartDelay");
+    }
+
+    IEnumerator StartDelay()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        yield return new WaitForSeconds(4);
+
+    }
 
     public void StartGame()
     {
         beginGame.gameObject.SetActive(false);
         beginGameText.gameObject.SetActive(false);
         buttons.gameObject.SetActive(true);
+
+        logo.gameObject.SetActive(false);
     }
 
-    // Start is called before the first frame update
-    void Start()
+    public void BeginGame()
     {
-        
+        SceneManager.LoadScene("LandOfLiving");
+    
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OptionMenu()
     {
-        
+
     }
+
+    public void PlayCredits()
+    {
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    
 }

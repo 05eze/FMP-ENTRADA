629efe5f8807909e27a6811366c652e74467f091
05eze <esintim2022@elam.co.uk>
2024-05-02 10:42:56 +0100

    Issues with the cutscene animator, may need to discard it for now to make time for other interactions

--- Assets/Scripts/Cutscene1.cs
+++ Assets/Scripts/Cutscene1.cs
@@ -8,28 +8,48 @@ public class Cutscene1 : MonoBehaviour
     public GameObject santaMuerteSpeech;
     public GameObject MainCam;
     public GameObject CutsceneCam;
+
+    public Animator animator_cam;
+    private bool IsOpen;
+
     // Start is called before the first frame update
 
 
     private void Start()
     {
-        
-       
-        FindAnyObjectByType<PlayerMovement>().enabled = false;
-        StartCutscene();
-        
+
+
+        //FindAnyObjectByType<PlayerMovement>().enabled = false;
+
+
+
     }
     // Update is called once per frame
-    IEnumerator StartCutscene()
+
+    private void OnTriggerEnter(Collider other)
     {
-        yield return new WaitForSeconds(2);
-        laManoSpeech.SetActive(true);
-        yield return new WaitForSeconds(1.5f);
-        laManoSpeech.SetActive(false);
-        yield return new WaitForSeconds(3);
-        santaMuerteSpeech.SetActive(true);
-        yield return new WaitForSeconds(5.4f);
-        MainCam.SetActive(true);
-        FindAnyObjectByType<PlayerMovement>().enabled = true;
+       
+        animator_cam.SetBool("IsOpen", true);
+        IsOpen = true;
     }
+
+
+    private void Update()
+    {
+       // if (animator_cam != null)
+      //  {
+            animator_cam.SetBool("IsOpen", false);
+            MainCam.SetActive(true);
+            CutsceneCam.SetActive(false);
+     //  }
+
+     
+       
+        
+    }
+
+   
+
+
+    
 }
